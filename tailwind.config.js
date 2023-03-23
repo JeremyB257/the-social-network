78e578fc03be1c53b8e8e92715bf188985e9ee49
Matthieu Mota <matthieumota@gmail.com>
2023-03-23 13:25:28 +0100

    Install Webpack Encore and Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './assets/**/*.js',
+    './templates/**/*.html.twig',
+    './vendor/symfony/twig-bridge/Resources/views/Form/*.html.php',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
