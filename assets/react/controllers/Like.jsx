78e062eda9bb71dc84a12c5dfbbbf93af98b554c
Matthieu Mota <matthieumota@gmail.com>
2023-03-27 17:28:46 +0200

    Like and comment with React

--- assets/react/controllers/Like.jsx
+++ assets/react/controllers/Like.jsx
@@ -1,9 +1,33 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 
-export default function ({ id, isLiked }) {
+export default function ({ id, isLiked, baseComments }) {
     const [liked, setLiked] = useState(isLiked);
     const [show, setShow] = useState(false);
+
+    const [data, setData] = useState({content: ''});
+    const [errors, setErrors] = useState({});
+    const [success, setSuccess] = useState(false);
+
+    const [comments, setComments] = useState(baseComments);
+
+    const handleChange = (event) => {
+        setData({ ...data, ...{ [event.target.name]: event.target.value } });
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        setErrors({});
+
+        axios.post(`/api/comment/create/${id}`, data)
+            .then(response => {
+                setSuccess(response.data.message);
+                setComments([ { ...response.data.comment }, ...comments ]);
+                setData({ content: '' });
+            })
+            .catch(errors => setErrors(errors.response.data));
+    }
 
     const like = () => {
         axios.get(`/like/${id}`).then(response => {
@@ -25,17 +49,33 @@ export default function ({ id, isLiked }) {
             </div>
 
             {show && <div>
-                <form className="mt-3">
+                {success && <p className="text-emerald-500">{success}</p>}
+                <form className="mt-3" onSubmit={handleSubmit}>
                     <div className="flex items-end">
                         <div className="flex-grow">
                             <label htmlFor="content" className="block text-gray-800 text-sm font-semibold required">Commentaire</label>
-                            <input type="text" id="content" name="content" className="mt-1 w-full rounded-lg border-gray-200" />
+                            <input type="text" id="content" name="content" value={data.content} onChange={handleChange} className="mt-1 w-full rounded-lg border-gray-200" />
+                            {errors.content && <p className="text-red-500">{errors.content}</p>}
                         </div>
                         <div className="ml-4">
                             <button className="inline-block px-6 py-2 bg-blue-500 hover:bg-blue-600 rounded text-white shadow-md duration-300">Publier</button>
                         </div>
                     </div>
                 </form>
+
+                <div className="mt-4">
+                    {comments.map(comment =>                            
+                        <div className="flex mb-4">
+                            <img className="w-8 h-8 rounded-full shadow-md mr-3 object-cover" src={comment.avatar} alt={comment.firstname} />
+                
+                            <div>
+                                <h2 className="font-bold text-gray-800">{comment.firstname}</h2>
+                                <p>{comment.content}</p>
+                                <p className="text-xs text-gray-400">{comment.created_at}</p>
+                            </div>
+                        </div>
+                    )}
+                </div>
             </div>}
         </div>
     );
--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -6,10 +6,13 @@ use App\Entity\Comment;
 use App\Entity\Post;
 use App\Form\CommentType;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Bundle\TimeBundle\DateTimeFormatter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Asset\Packages;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class PostController extends AbstractController
 {
@@ -37,6 +40,50 @@ class PostController extends AbstractController
         return $this->render('post/index.html.twig', [
             'post' => $post,
             'form' => $form,
+        ]);
+    }
+
+    #[Route('/api/comment/create/{id}', name: 'app_api_comment_create', methods: ['POST'])]
+    #[IsGranted('ROLE_USER')]
+    public function create(
+        Request $request,
+        DateTimeFormatter $dateTimeFormatter,
+        Packages $packages,
+        EntityManagerInterface $manager,
+        Post $post
+    ) {
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment, ['csrf_protection' => false]);
+        $form->submit(json_decode($request->getContent(), true));
+        // $form = $this->createFormBuilder(null, ['csrf_protection' => false])
+        //     ->add('name', null, [
+        //         'constraints' => [new NotBlank()]
+        //     ])->getForm();
+        
+
+        if (!$form->isValid()) {
+            $errors = [];
+
+            foreach ($form->getErrors(true) as $error) {
+                $errors[$error->getOrigin()->getName()] = $error->getMessage();
+            }
+
+            return $this->json($errors, 422);
+        }
+
+        $comment->setPost($post);
+        $comment->setCreator($this->getUser());
+        $manager->persist($comment);
+        $manager->flush();
+
+        return $this->json([
+            'message' => 'Merci !',
+            'comment' => [
+                'content' => $comment->getContent(),
+                'avatar' => $comment->getCreator() ? $packages->getUrl($comment->getCreator()->getAvatar()) : null,
+                'firstname' => $comment->getCreator() ? $comment->getCreator()->getFirstname() : null,
+                'created_at' => $dateTimeFormatter->formatDiff($comment->getCreatedAt(), new \DateTime()),
+            ],
         ]);
     }
 }
